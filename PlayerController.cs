a034b8c1fcfb65d9d01c9b006b6a575f6d02bafb
Ren Vael <vael_rin@yahoo.com>
2017-04-23 20:47:55 -0400

    Create PlayerController.cs

---
+++ PlayerController.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary 
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+
+	public GameObject shot;
+	public GameObject shotSpawn;
+	public float fireRate;
+
+	private float nextFire;
+	private GameObject shotFired;
+
+	void Update ()
+	{
+		if (Input.GetButton("Jump") && Time.time > nextFire) 
+		{
+			shotFired = Instantiate(shot) as GameObject;
+			nextFire = Time.time + fireRate;
+			// Game Object clone =
+			shotFired.transform.position = shotSpawn.transform.position;
+			GetComponent<AudioSource>().Play ();
+		}
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		GetComponent<Rigidbody>().velocity = movement * speed;
+
+		GetComponent<Rigidbody>().position = new Vector3
+			(
+				Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 
+				0.0f, 
+				Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+			);
+
+		GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+	}
+}
