8cc44ffb6f5e85fc2e7e92db926248146e3c29c2
Ren Vael <vael_rin@yahoo.com>
2017-04-23 20:46:46 -0400

    Create Mover.cs

---
+++ Mover.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour 
+{
+	public float speed;
+	public Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.velocity = transform.forward * speed;
+	}
+}
